1c27eba4897b5c944ba8db1db6d90602ef0d8e32
pierthodo <pierthodo@gmail.com>
2022-04-14 14:17:54 +0100

    Update __init__.py

--- mbrl/env/__init__.py
+++ mbrl/env/__init__.py
@@ -3,3 +3,4 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 from . import cartpole_continuous, reward_fns, termination_fns
+from .cartpole_continuous import CartPoleEnv
