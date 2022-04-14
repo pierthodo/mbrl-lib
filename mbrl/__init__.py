1f57329b004e611238cd27292a8d659e5a21e5e1
pierthodo <pierthodo@gmail.com>
2022-04-14 14:18:24 +0100

    Update __init__.py

--- mbrl/__init__.py
+++ mbrl/__init__.py
@@ -3,3 +3,11 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 __version__ = "0.2.0.dev4"
+from gym.envs.registration import register
+
+register(
+    id='mbrl/CartPole-v0',
+    entry_point='mbrl.env:CartPoleEnv',
+    max_episode_steps=200,
+)
+
